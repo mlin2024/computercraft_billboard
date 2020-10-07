d8f1f8c9b3f16113fc65dfb0b2bc39abd52e24c7
mlin2024 <31043603+mlin2024@users.noreply.github.com>
2020-10-07 16:31:56 -0400

    Made it run constantly until told to shutdown

--- display.lua
+++ display.lua
@@ -1,9 +1,21 @@
 rednet.open("top")
-
-io.write("What message do you want to display?")
-message = io.read()
-rednet.broadcast(message)
-
-io.write("Enter a speed between 1 (slow) and 10 (fast)")
-speed = io.read()
-rednet.broadcast(speed)
+while true do
+    io.write("What message do you want to display? (\"shutdown\" to exit)\n")
+    message = io.read()
+    rednet.broadcast(message)
+    if message == "shutdown" then
+        return 0
+    end
+    while true do
+        io.write("Enter a speed between 1 (slow) and 10 (fast)\n")
+        speed = io.read()
+        speeed = tonumber(speed)
+        if speeed < 0 or speeed > 10 then
+            print("Invalid input.\n")
+        else
+            rednet.broadcast(speed)
+            break
+        end
+    end
+end   
+rednet.close()
